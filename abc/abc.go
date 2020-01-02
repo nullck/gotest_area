814e96f95ab695758ad5ef6e538e396593074b9a
Thiago Dias <nullck@gmail.com>
2020-01-02 17:33:18 -0300

    abc package

---
+++ abc/abc.go
@@ -0,0 +1,11 @@
+package abc
+
+import "fmt"
+
+// HelloWorld is a function that belongs
+// to a package called abc I can create N
+// package in the same repo
+
+func HelloWorld() {
+	fmt.Println("hello world")
+}
